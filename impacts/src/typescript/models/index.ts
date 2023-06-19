3418f31f3ba5e81fa675d856a0f552eb9f38d94f
donatpeter <donat@getgreenspark.com>
2023-06-19 16:39:36 +0200

    Create impact purchase by value

--- impacts/src/typescript/models/index.ts
+++ impacts/src/typescript/models/index.ts
@@ -1,3 +1,5 @@
 export * from './impact-dto';
 export * from './impact-purchase';
+export * from './impact-purchase-by-value';
+export * from './impact-purchase-detail';
 export * from './metadata';
