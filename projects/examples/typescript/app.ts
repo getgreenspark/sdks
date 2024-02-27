386771c0f1eb6e8da48d14cd61820a388430b7b6
donatpeter <donat@getgreenspark.com>
2024-02-27 17:17:34 +0100

    fix type issue in example

--- projects/examples/typescript/app.ts
+++ projects/examples/typescript/app.ts
@@ -3,7 +3,7 @@ import {Project, ProjectsApi} from "@getgreenspark/projects"
 async function getProjects() {
     try {
         const projectsApi = new ProjectsApi({basePath: "https://sandbox.getgreenspark.com", apiKey: "<YOUR_API_KEY>"})
-        const {data}: Project[] = await projectsApi.getProjects()
+        const {data} = await projectsApi.getProjects()
         console.log(data);
     } catch (error) {
         console.error(error);
