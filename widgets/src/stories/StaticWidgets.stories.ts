a392dd4bdcf847cb36e02b0eea85156748468ed2
donatpeter <donat@getgreenspark.com>
2025-03-04 12:32:42 +0100

    change storybook

--- widgets/src/stories/StaticWidgets.stories.ts
+++ widgets/src/stories/StaticWidgets.stories.ts
@@ -1,67 +1,90 @@
-import { AVAILABLE_LOCALES, STATIC_WIDGET_STYLES, WIDGET_COLORS } from '@/constants'
+import { AVAILABLE_LOCALES, WIDGET_COLORS } from '@/constants'
 import type { StoryObj, Meta } from '@storybook/html'
-import type GreensparkWidgets from '@/index'
-import { ConnectionHandler } from '@/network'
-import { PREVIEWS_USER } from '@/stories/users'
+import GreensparkWidgets from '@/index'
+import { PREVIEWS_USER, STORE_USERS } from '@/stories/users'
+import { createWidgetPage } from './Widgets'
+import type { ByPercentageWidgetParams, StaticWidgetStyle } from '../interfaces'
+import type { Widget } from '../widgets/base'
+const USERS = { PREVIEW: PREVIEWS_USER, SINGULAR: STORE_USERS.SINGULAR }
 
 const meta = {
   title: 'Widget SDK/Static Widgets',
   tags: ['autodocs'],
   render: (args) => {
-    const { locale } = args
-    const container = document.createElement('div')
+    const { locale, style, user } = args
+
+    const widgets = new GreensparkWidgets({
+      apiKey: USERS[user].apiKey,
+      integrationSlug: USERS[user].integrationSlug,
+      locale,
+    })
 
     const variants = [{ version: 'v2' }]
 
-    const handler = new ConnectionHandler({
-      apiKey: PREVIEWS_USER.apiKey,
-      locale: locale,
-      isShopifyIntegration: false,
-    })
-
-    const article = document.createElement('article')
-    article.style.maxWidth = '100%'
-
-    for (const color of WIDGET_COLORS) {
-      for (const variant of variants) {
-        handler
-          .fetchStaticWidget({
-            color,
-            version: variant.version as unknown as 'v2',
-            style: 'simplified',
-          })
-          .then((response) => {
-            const widgetContainer = document.createElement('div')
-            widgetContainer.style.maxWidth = '100%'
-            widgetContainer.style.margin = '20px 0'
-            widgetContainer.innerHTML = response.data
-            container.appendChild(widgetContainer)
-          })
-      }
+    let widget: Widget
+    switch (style) {
+      case 'default':
+        widget = widgets.static({ color: 'blue', style })
+        return createWidgetPage('static', widget, WIDGET_COLORS, variants)
+      case 'simplified':
+        widget = widgets.static({ color: 'blue', style })
+        return createWidgetPage('static', widget, WIDGET_COLORS, variants)
+      case 'rounded':
+        widget = widgets.static({ color: 'blue', style })
+        return createWidgetPage('static', widget, WIDGET_COLORS, variants)
     }
-
-    return container
   },
   argTypes: {
+    user: {
+      control: { type: 'select' },
+      options: Object.keys(USERS),
+    },
     locale: {
       control: { type: 'select' },
       options: AVAILABLE_LOCALES,
     },
   },
   args: {
+    user: 'SINGULAR',
     locale: 'en',
   },
 } satisfies Meta<
   GreensparkWidgets & {
+    style: StaticWidgetStyle
     locale: (typeof AVAILABLE_LOCALES)[number]
+    user: keyof typeof USERS
   }
 >
 
 export default meta
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
-export const Static: StoryObj<{
-  widgetType: keyof GreensparkWidgets
+export const Default: StoryObj<{
+  widgetArgs: ByPercentageWidgetParams
+  style: StaticWidgetStyle
 }> = {
   argTypes: {},
+  args: {
+    style: 'default',
+  },
+}
+
+export const Simplified: StoryObj<{
+  widgetArgs: ByPercentageWidgetParams
+  style: StaticWidgetStyle
+}> = {
+  argTypes: {},
+  args: {
+    style: 'simplified',
+  },
+}
+
+export const Rounded: StoryObj<{
+  widgetArgs: ByPercentageWidgetParams
+  style: StaticWidgetStyle
+}> = {
+  argTypes: {},
+  args: {
+    style: 'rounded',
+  },
 }
