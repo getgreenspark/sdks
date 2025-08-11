546f0c30c9ba715a5976a5b3edff9ecf837925cd
Tamas Gerenyi <getamas04@gmail.com>
2025-08-11 21:39:45 +0200

    fix widget error

--- widgets-shopify/src/index.ts
+++ widgets-shopify/src/index.ts
@@ -219,7 +219,7 @@ function runGreenspark() {
           const order = parseCart(updatedCart)
           if (order.lineItems.length <= 0) return
           return window
-            .greensparkCartWidget!.render({ order }, containerSelector)
+            .greensparkCartWidget!.render({ order })
             .then(() => {
               setupPopupMove()
               if (typeof prevChecked === 'boolean') {
