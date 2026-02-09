cbace649c62e22a14135106cadab9afc99f82567
donatpeter <donat@getgreenspark.com>
2026-02-09 16:35:04 +0100

    update

--- widgets-bigcommerce/src/config.ts
+++ widgets-bigcommerce/src/config.ts
@@ -6,8 +6,8 @@ const isDevStore =
   (window.location?.hostname?.includes('greenspark-development-store') ?? false)
 
 export const widgetUrl = isDevStore
-  ? 'https://cdn.getgreenspark.com/scripts/widgets%402.6.1-0.js'
-  : 'https://cdn.getgreenspark.com/scripts/widgets%402.6.1-0.js'
+  ? 'https://cdn.getgreenspark.com/scripts/widgets%402.6.1-1.js'
+  : 'https://cdn.getgreenspark.com/scripts/widgets%402.6.1-1.js'
 
 export function getScriptSrc(): string | undefined {
   if (typeof document === 'undefined') return undefined
