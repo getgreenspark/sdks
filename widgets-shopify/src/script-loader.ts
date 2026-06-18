f9ea10c6edf5ed151d8e471cf82250e8270a9e99
Tamas Gerenyi <getamas04@gmail.com>
2026-06-18 16:39:38 +0200

    add dev store check to script loader

--- widgets-shopify/src/script-loader.ts
+++ widgets-shopify/src/script-loader.ts
@@ -1,6 +1,10 @@
+import { isGreensparkDevStore } from './config'
 import { err } from './debug'
 
-export const widgetUrl = 'https://cdn.getgreenspark.com/scripts/widgets%40latest.js'
+const isDevStore = isGreensparkDevStore(window.location.hostname)
+export const widgetUrl = isDevStore
+  ? 'https://cdn.getgreenspark.com/scripts/widgets%402.6.3.js'
+  : 'https://cdn.getgreenspark.com/scripts/widgets%40latest.js'
 
 const MAX_SCRIPT_RETRIES = 5
 
